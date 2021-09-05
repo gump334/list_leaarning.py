1df42c2a3ddc652c7470b3cae553f5e669f6d96f
Terrell <gumpfinest3341983@gmail.com>
2021-09-05 15:58:35 +0000

    added more list content

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 
 morning_call = ["Camille", "David", "T.J"]
 
-print(morning_call[0])
-print()
-
 # looping through list
 for x in morning_call:
-  print(x +"\n")
+  print(f"{x.upper()} It's a pleasure to have you on this call today. \n")
+print("Thank you all for joining the morning call\n")
 
 # appending to list
 morning_call.append("Terrell")
@@ -24,6 +22,51 @@ print()
 for x in morning_call[:]:
   if x.startswith("g"):
     morning_call.remove(x)
-print("No words start with the letter thats inputted")
+print("No words start with the letter thats inputted\n")
+morning_call.sort() # sort my list from Caps to lowercase order
 print(morning_call)
+print()
+
+#using range method for creating a list of numbers.
+# Range always end 1 number less then the number that you put in the brackets
+for num in range(6):
+  print(num)
+
+numbers = list(range(1,11))
+print(numbers)
+print()
+
+# using range to get even numbers
+even_numbers = list(range(2, 21, 2))
+print(even_numbers)
+print()
+
+#square root 
+squares = []
+for value in range(1,11):
+  square = value ** 2
+  squares.append(square)
+print(squares)
+print()
+
+#power of 2
+powers = []
+for value in range(1,11):
+  powers.append(value*value)
+print(powers)
+print()
+
+#print the values of the power output to dollar signs. let's get money
+for num in powers:
+  print(num * "$")
+
+# getting the min, man and sum value of the  powers lists
+print(min(powers))
+print(max(powers))
+print(sum(powers))
+print()
+
+#list comprehensions
+listpowers =  [value * value for value in range(1,11)]
+print(listpowers)
 
